20c1459acba99afcea9e09bc2ae792a0fd3413f6
wiktor <wikcioo@gmail.com>
2022-11-11 17:40:29 +0100

    Create users controller.

---
+++ WebApi/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+using Application.LogicInterfaces;
+using Domain.DTOs;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IUserLogic _userLogic;
+
+    public UsersController(IUserLogic userLogic)
+    {
+        _userLogic = userLogic;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<User>> CreateAsync(UserCreateDto dto)
+    {
+        try
+        {
+            var user = await _userLogic.CreateAsync(dto);
+            return Created($"/users/{user.Username}", user);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+}
