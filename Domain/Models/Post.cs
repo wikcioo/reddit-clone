17f53571dad3f8635b4ee1ead4d2e28f7ba683b6
wiktor <wikcioo@gmail.com>
2022-11-11 18:16:55 +0100

    Add post model.

---
+++ Domain/Models/Post.cs
@@ -0,0 +1,16 @@
+namespace Domain.Models;
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public User Owner { get; set; }
+
+    public Post(string title, string content, User owner)
+    {
+        Title = title;
+        Content = content;
+        Owner = owner;
+    }
+}
