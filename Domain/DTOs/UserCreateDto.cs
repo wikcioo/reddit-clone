b083f1c7a9b9e8c858f26aea0d9ea38946270e21
wiktor <wikcioo@gmail.com>
2022-11-11 17:31:11 +0100

    Add create and login user dtos.

---
+++ Domain/DTOs/UserCreateDto.cs
@@ -0,0 +1,8 @@
+namespace Domain.DTOs;
+
+public class UserCreateDto
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Role     { get; set; } = string.Empty;
+}
