60b4850205f230f612f5c3aab7f2e6be88095298
wiktor <wikcioo@gmail.com>
2022-11-11 17:23:26 +0100

    Add user model.

---
+++ Domain/Models/User.cs
@@ -0,0 +1,8 @@
+namespace Domain.Models;
+
+public class User
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Role     { get; set; } = string.Empty;
+}
