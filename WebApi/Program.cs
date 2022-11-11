c2b8bf6343cfc5ffeda47f603c495f3ac4ad36f1
wiktor <wikcioo@gmail.com>
2022-11-11 17:44:37 +0100

    Add services and use cors.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,9 @@
+using Application.DaoInterfaces;
+using Application.LogicImplementations;
+using Application.LogicInterfaces;
+using FileStorage;
+using FileStorage.DAOs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +13,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<FileContext>();
+builder.Services.AddScoped<IUserDao, UserFileDao>();
+builder.Services.AddScoped<IUserLogic, UserLogic>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +25,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true) // allow any origin
+    .AllowCredentials());
 
 app.UseHttpsRedirection();
 
