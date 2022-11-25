a6343753123e17b2fc9a8e79ff5d89f9dbfa95cc
wikcioo <wikcioo@gmail.com>
2022-11-25 15:29:39 +0100

    Create efc context.

---
+++ EfcStorage/EfcContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfcStorage;
+
+public class EfcContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Post> Posts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source = ../EfcStorage/RedditClone.db");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasKey(user => user.Username);
+        modelBuilder.Entity<Post>().HasKey(post => post.Id);
+    }
+}
