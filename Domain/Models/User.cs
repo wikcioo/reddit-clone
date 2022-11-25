a9d37f04b80f484a796400c47d47155628b2e128
wikcioo <wikcioo@gmail.com>
2022-11-25 15:30:52 +0100

    Add a collection of Posts required by efc.

--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -1,7 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace Domain.Models;
 
 public class User
 {
     public string Username { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
+    
+    [JsonIgnore]
+    public ICollection<Post> Posts { get; set; }
 }
